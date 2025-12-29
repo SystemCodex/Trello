31cae866554230194b5c36e800f45b99835bed3d
SystemCodex <hebpsantos07@gmail.com>
2025-12-29 01:10:03 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
+// Tu configuración específica
+const firebaseConfig = {
+    apiKey: "AIzaSyAQnrIODc_2Qv_Snow02X-Sq8_PHwMoRVk",
+    authDomain: "trello-d2532.firebaseapp.com",
+    projectId: "trello-d2532",
+    storageBucket: "trello-d2532.firebasestorage.app",
+    messagingSenderId: "630892154656",
+    appId: "1:630892154656:web:1d0dfce355216a1d879145",
+    measurementId: "G-4L4P7E6TZC"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
